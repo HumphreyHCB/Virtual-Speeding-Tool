8059cf5bae8b29ec57f3e384f5e9b8bad76298e5
Humphrey Burchell <humphreyburchell@gmail.com>
2022-11-02 21:54:43 +0000

    add Java code example

---
+++ JavaExample.java
@@ -0,0 +1,19 @@
+public class JavaExample {
+    public static void main(String[] args) {
+        testMaths();
+        myprint();
+    }
+
+    public static void myprint() {
+        System.out.println("Hello");
+        System.out.println("This is my Print");
+    }
+
+    public static void testMaths() {
+        int x = 7;
+        int y = 12;
+
+        int z = x + y;
+    }
+    
+}
