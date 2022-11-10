275133038885a526d45d7a8d46fca2c6171e8962
Humphrey Burchell <humphreyburchell@gmail.com>
2022-11-10 23:02:47 +0000

    added more to test file

--- testjsfile.js
+++ testjsfile.js
@@ -2,6 +2,15 @@ function testA(params) {
     console.log("print");
     console.log("print");
 }
+function testB(params) {
+    console.log("print 2");
+    console.log("print 2");
+    testC();
+}
+function testC(params) {
+    console.log("print 3");
+    console.log("print 3");
+}
 
 testA();
-testA();
+testB();
