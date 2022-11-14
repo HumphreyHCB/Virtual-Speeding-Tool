390c63dae75b2295f7f8a433294806ad93f01dd1
Humphrey Burchell <humphreyburchell@gmail.com>
2022-11-14 22:22:24 +0000

    added bash script to run multiple benchmarks

---
+++ benchmarksRunner.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+./VStool node harness.js towers 1000 1 
+./VStool node harness.js towers 1000 1 
+./VStool node harness.js towers 1000 1 
+./VStool node harness.js towers 2000 1 
+./VStool node harness.js towers 2000 1 
+./VStool node harness.js towers 2000 1 
+./VStool node harness.js towers 2000 1 
+./VStool node harness.js towers 2000 1 
+./VStool node harness.js towers 2000 1 
+./VStool node harness.js towers 2000 1 
+./VStool node harness.js towers 2000 1 
+./VStool node harness.js towers 2000 1 
