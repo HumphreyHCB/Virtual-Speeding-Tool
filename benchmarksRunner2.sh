3ab7d3b0952a9eca98a0f53cf7f8fd434b55f45f
Humphrey Burchell <humphreyburchell@gmail.com>
2022-11-22 12:26:22 +0000

    bad bubbles data

---
+++ benchmarksRunner2.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+./VStool node --Virtually-Speeding-Tool.speed-up-Method="No-Method" harness.js bubbles 500 1 
+./VStool node --Virtually-Speeding-Tool.speed-up-Method="bubbleSort" harness.js bubbles 500 1
+./VStool node --Virtually-Speeding-Tool.speed-up-Method="badbubble" harness.js bubbles 500 1 
+
