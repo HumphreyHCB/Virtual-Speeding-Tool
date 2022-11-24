2d12f300e34c126a1d81b9f02710eb79b2f6ca13
Humphrey Burchell <humphreyburchell@gmail.com>
2022-11-24 13:13:39 +0000

    changes to becnhmark runner

--- benchmarksRunner.sh
+++ benchmarksRunner.sh
@@ -1,29 +1,29 @@
 #!/usr/bin/env bash
 
-./VStool node --Virtually-Speeding-Tool.speed-up-Method="No-Method" --Virtually-Speeding-Tool.Slowdown-amount=1000 harness.js sortingTest 1000 1 
-./VStool node --Virtually-Speeding-Tool.speed-up-Method="bubbleSort" --Virtually-Speeding-Tool.Slowdown-amount=1000 harness.js sortingTest 1000 1
-./VStool node --Virtually-Speeding-Tool.speed-up-Method="selectionSort" --Virtually-Speeding-Tool.Slowdown-amount=1000 harness.js sortingTest 1000 1 
-./VStool node --Virtually-Speeding-Tool.speed-up-Method="insertionSort" --Virtually-Speeding-Tool.Slowdown-amount=1000 harness.js sortingTest 1000 1 
-./VStool node --Virtually-Speeding-Tool.speed-up-Method="mergeSort" --Virtually-Speeding-Tool.Slowdown-amount=1000 harness.js sortingTest 1000 1 
-./VStool node --Virtually-Speeding-Tool.speed-up-Method="quickSort" --Virtually-Speeding-Tool.Slowdown-amount=1000 harness.js sortingTest 1000 1 
-./VStool node --Virtually-Speeding-Tool.speed-up-Method="heapSort" --Virtually-Speeding-Tool.Slowdown-amount=1000 harness.js sortingTest 1000 1
-echo -e "\n" >> TestData/CounterApproach/SortingDump1StatementTag.txt 
+./VStool node --Virtually-Speeding-Tool.speed-up-Method="No-Method" --Virtually-Speeding-Tool.Slowdown-amount=10 harness.js sortingTest 100 1 
+#./VStool node --Virtually-Speeding-Tool.speed-up-Method="bubbleSort" --Virtually-Speeding-Tool.Slowdown-amount=10 harness.js sortingTest 100 1
+#./VStool node --Virtually-Speeding-Tool.speed-up-Method="selectionSort" --Virtually-Speeding-Tool.Slowdown-amount=10 harness.js sortingTest 100 1 
+#./VStool node --Virtually-Speeding-Tool.speed-up-Method="insertionSort" --Virtually-Speeding-Tool.Slowdown-amount=10 harness.js sortingTest 100 1 
+./VStool node --Virtually-Speeding-Tool.speed-up-Method="mergeSort" --Virtually-Speeding-Tool.Slowdown-amount=10 harness.js sortingTest 100 1 
+./VStool node --Virtually-Speeding-Tool.speed-up-Method="quickSort" --Virtually-Speeding-Tool.Slowdown-amount=10 harness.js sortingTest 100 1 
+./VStool node --Virtually-Speeding-Tool.speed-up-Method="heapSort" --Virtually-Speeding-Tool.Slowdown-amount=10 harness.js sortingTest 100 1
+echo -e "\n" >> TestData/CounterApproach/SortingDump5.txt 
 
-./VStool node --Virtually-Speeding-Tool.speed-up-Method="No-Method" --Virtually-Speeding-Tool.Slowdown-amount=10000 harness.js sortingTest 1000 1 
-./VStool node --Virtually-Speeding-Tool.speed-up-Method="bubbleSort" --Virtually-Speeding-Tool.Slowdown-amount=10000 harness.js sortingTest 1000 1
-./VStool node --Virtually-Speeding-Tool.speed-up-Method="selectionSort" --Virtually-Speeding-Tool.Slowdown-amount=10000 harness.js sortingTest 1000 1 
-./VStool node --Virtually-Speeding-Tool.speed-up-Method="insertionSort" --Virtually-Speeding-Tool.Slowdown-amount=10000 harness.js sortingTest 1000 1 
-./VStool node --Virtually-Speeding-Tool.speed-up-Method="mergeSort" --Virtually-Speeding-Tool.Slowdown-amount=10000 harness.js sortingTest 1000 1 
-./VStool node --Virtually-Speeding-Tool.speed-up-Method="quickSort" --Virtually-Speeding-Tool.Slowdown-amount=10000 harness.js sortingTest 1000 1 
-./VStool node --Virtually-Speeding-Tool.speed-up-Method="heapSort" --Virtually-Speeding-Tool.Slowdown-amount=10000 harness.js sortingTest 1000 1
-echo -e "\n" >> TestData/CounterApproach/SortingDump1StatementTag.txt
+# ./VStool node --Virtually-Speeding-Tool.speed-up-Method="No-Method" --Virtually-Speeding-Tool.Slowdown-amount=100 harness.js sortingTest 1000 1 
+# ./VStool node --Virtually-Speeding-Tool.speed-up-Method="bubbleSort" --Virtually-Speeding-Tool.Slowdown-amount=100 harness.js sortingTest 1000 1
+# ./VStool node --Virtually-Speeding-Tool.speed-up-Method="selectionSort" --Virtually-Speeding-Tool.Slowdown-amount=100 harness.js sortingTest 1000 1 
+# ./VStool node --Virtually-Speeding-Tool.speed-up-Method="insertionSort" --Virtually-Speeding-Tool.Slowdown-amount=100 harness.js sortingTest 1000 1 
+# ./VStool node --Virtually-Speeding-Tool.speed-up-Method="mergeSort" --Virtually-Speeding-Tool.Slowdown-amount=100 harness.js sortingTest 1000 1 
+# ./VStool node --Virtually-Speeding-Tool.speed-up-Method="quickSort" --Virtually-Speeding-Tool.Slowdown-amount=100 harness.js sortingTest 1000 1 
+# ./VStool node --Virtually-Speeding-Tool.speed-up-Method="heapSort" --Virtually-Speeding-Tool.Slowdown-amount=100 harness.js sortingTest 1000 1
+# echo -e "\n" >> TestData/CounterApproach/SortingDump3.txt
 
 
-./VStool node --Virtually-Speeding-Tool.speed-up-Method="No-Method" --Virtually-Speeding-Tool.Slowdown-amount=100000 harness.js sortingTest 1000 1 
-./VStool node --Virtually-Speeding-Tool.speed-up-Method="bubbleSort" --Virtually-Speeding-Tool.Slowdown-amount=100000 harness.js sortingTest 1000 1
-./VStool node --Virtually-Speeding-Tool.speed-up-Method="selectionSort" --Virtually-Speeding-Tool.Slowdown-amount=100000 harness.js sortingTest 1000 1 
-./VStool node --Virtually-Speeding-Tool.speed-up-Method="insertionSort" --Virtually-Speeding-Tool.Slowdown-amount=100000 harness.js sortingTest 1000 1 
-./VStool node --Virtually-Speeding-Tool.speed-up-Method="mergeSort" --Virtually-Speeding-Tool.Slowdown-amount=100000 harness.js sortingTest 1000 1 
-./VStool node --Virtually-Speeding-Tool.speed-up-Method="quickSort" --Virtually-Speeding-Tool.Slowdown-amount=100000 harness.js sortingTest 1000 1 
-./VStool node --Virtually-Speeding-Tool.speed-up-Method="heapSort" --Virtually-Speeding-Tool.Slowdown-amount=100000 harness.js sortingTest 1000 1
-echo -e "\n" >> TestData/CounterApproach/SortingDump1StatementTag.txt
+# ./VStool node --Virtually-Speeding-Tool.speed-up-Method="No-Method" --Virtually-Speeding-Tool.Slowdown-amount=100000 harness.js sortingTest 1000 1 
+# ./VStool node --Virtually-Speeding-Tool.speed-up-Method="bubbleSort" --Virtually-Speeding-Tool.Slowdown-amount=100000 harness.js sortingTest 1000 1
+# ./VStool node --Virtually-Speeding-Tool.speed-up-Method="selectionSort" --Virtually-Speeding-Tool.Slowdown-amount=100000 harness.js sortingTest 1000 1 
+# ./VStool node --Virtually-Speeding-Tool.speed-up-Method="insertionSort" --Virtually-Speeding-Tool.Slowdown-amount=100000 harness.js sortingTest 1000 1 
+# ./VStool node --Virtually-Speeding-Tool.speed-up-Method="mergeSort" --Virtually-Speeding-Tool.Slowdown-amount=100000 harness.js sortingTest 1000 1 
+# ./VStool node --Virtually-Speeding-Tool.speed-up-Method="quickSort" --Virtually-Speeding-Tool.Slowdown-amount=100000 harness.js sortingTest 1000 1 
+# ./VStool node --Virtually-Speeding-Tool.speed-up-Method="heapSort" --Virtually-Speeding-Tool.Slowdown-amount=100000 harness.js sortingTest 1000 1
+# echo -e "\n" >> TestData/CounterApproach/SortingDump3.txt
