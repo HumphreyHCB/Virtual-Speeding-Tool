45da09ba1e5b40da4b75070cac14366b68b11664
Humphrey Burchell <humphreyburchell@gmail.com>
2022-12-05 20:20:08 +0000

    returned js launcher back to normal

--- tests/java/JsJavaLauncher.java
+++ tests/java/JsJavaLauncher.java
@@ -7,24 +7,14 @@ public class JsJavaLauncher {
         Launch();
     }
 
-    // private static void Launch() {
-    //     Context ctx = Context.newBuilder("js", "java")
-    //             .allowAllAccess(true)
-    //             .option("Virtually-Speeding-Tool", "true")
-    //             .option("Virtually-Speeding-Tool.Slowdown-amount", "100000")
-    //             .option("Virtually-Speeding-Tool.speed-up-Method", "moveDisks")
-    //             .build();
-    //     ctx.eval("js", "let Harness = Polyglot.eval('java','code.Harness'); Harness.main(['Towers','10','1'])");
-    // }
     private static void Launch() {
         Context ctx = Context.newBuilder("js", "java")
                 .allowAllAccess(true)
-                .allowHostAccess(true)
                 .option("Virtually-Speeding-Tool", "true")
                 .option("Virtually-Speeding-Tool", "true")
                 .option("Virtually-Speeding-Tool.Slowdown-amount", "1000")
                 .option("Virtually-Speeding-Tool.speed-up-Method", "moveDisks")
                 .build();
-        ctx.eval("js", "let Harness = Polyglot.eval('java','code.Harness'); Harness.main(['Towers','10','1'])");
+        ctx.eval("js", "let Harness = Java.type('code.Harness'); Harness.main(['Towers','1','1'])");
     }
 }
