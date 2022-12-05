9ef89e4dcc9c495f62e94bf86640c8f8aff6a562
Humphrey Burchell <humphreyburchell@gmail.com>
2022-12-05 12:33:14 +0000

    add truffle option to bash

--- scripts/build-java.sh
+++ scripts/build-java.sh
@@ -3,8 +3,8 @@
 set -e
 
 
-javac -d classes tests/java/JsJavaLauncher.java
+javac -truffle -d classes tests/java/JsJavaLauncher.java
 
-javac -d classes tests/java/*.java
+javac -truffle -d classes tests/java/*.java
 
 
