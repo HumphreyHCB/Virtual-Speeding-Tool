80b71e424ee289bfb5544d03a414e2e2acad34d8
Humphrey Burchell <humphreyburchell@gmail.com>
2022-12-29 15:04:56 +0000

    added json jar to class path

--- scripts/build-java.sh
+++ scripts/build-java.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-javac -cp target/jars/json-20220924.jar -d classes srcs/java/tool/*.java srcs/java/launchers/*.java srcs/java/code/*.java
+javac -classpath target/jars/json-20220924.jar -d classes srcs/java/tool/*.java srcs/java/launchers/*.java srcs/java/code/*.java
 
 
 
--- scripts/tool-launcher.sh
+++ scripts/tool-launcher.sh
@@ -3,8 +3,8 @@
 PROJECT_ROOT_DIR="$(pwd)"
 JAVA_CLASS_PATH_DIR="${PROJECT_ROOT_DIR}/classes"
 SIMPLETOOL_JAR="${PROJECT_ROOT_DIR}/target/VStool.jar"
+JSON_JAR="${PROJECT_ROOT_DIR}/target/jars/json-20220924.jar"
 
-java \
-    --class-path "$JAVA_CLASS_PATH_DIR" \
+java --class-path "$JAVA_CLASS_PATH_DIR":"$JSON_JAR":. \
     "-Dtruffle.class.path.append=${SIMPLETOOL_JAR}" \
-    launchers.ToolLauncher badBubbles
+    launchers.ToolLauncher sortingtest
