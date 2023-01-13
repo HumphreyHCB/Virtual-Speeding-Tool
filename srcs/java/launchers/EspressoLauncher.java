44f0c5a0c0d1c0e89ec7a8a91750b707c3f1ea95
Humphrey Burchell <humphreyburchell@gmail.com>
2023-01-13 10:54:32 +0000

    added flags to reduce JIT warmup

--- srcs/java/launchers/EspressoLauncher.java
+++ srcs/java/launchers/EspressoLauncher.java
@@ -37,19 +37,25 @@ public class EspressoLauncher {
             .option("Virtually-Speeding-Tool.Slowdown-amount", slowdownAmount)
             .option("Virtually-Speeding-Tool.speed-up-Method", slowdownMethod)
             .option("java.Classpath", new JSONObject(Files.readString(Path.of("env.json"))).getString("Classes-Path"))
-            .option("java.MultiThreaded", "false") // JS is single-threaded
+            .option("java.MultiThreaded", "true")
+            .option("engine.MultiTier", "true")
+            .option("engine.DynamicCompilationThresholds", "false")
+            .option("engine.SingleTierCompilationThreshold", "253")
+            .option("engine.CompilationFailureAction", "ExitVM")
             .allowAllAccess(true)
             .allowExperimentalOptions(true)
             .build()) {
 
         Value bindings = ctx.getBindings("java");
         Value harness = bindings.getMember("tool.Harness");
-        harness.invokeMember("main", (Object) new Object[]{javaProgram, "500", "1", slowdownMethod, new JSONObject(Files.readString(Path.of("env.json"))).getString("write-Path")});       
+        harness.invokeMember("main", (Object) new Object[]{javaProgram, "50", "1", slowdownMethod, new JSONObject(Files.readString(Path.of("env.json"))).getString("write-Path")});       
 
         }catch (Exception e) {
             e.printStackTrace();
             // TODO: handle exception
         }
+
+        
     }
 }
 
