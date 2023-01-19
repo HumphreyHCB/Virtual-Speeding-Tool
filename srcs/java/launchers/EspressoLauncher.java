da77e9db6074b641b439dd63030562ce9f5d43c7
Humphrey Burchell <humphreyburchell@gmail.com>
2023-01-19 17:50:46 +0000

    gave launcher a method overload

--- srcs/java/launchers/EspressoLauncher.java
+++ srcs/java/launchers/EspressoLauncher.java
@@ -11,24 +11,10 @@ import org.json.JSONObject;
 
 public class EspressoLauncher {
     public static void main(String[] args) {
-        // args0 = slowdown-amount
-        // args1 = method
-        // args2 = java file
-        try(Context ctx = Context.newBuilder("java", "js")
-                .option("Virtually-Speeding-Tool", "true")
-                .option("java.Classpath", "/home/hburchell/Repos/Virtual-Speeding-Tool/classes")
-                .option("java.MultiThreaded", "false") // JS is single-threaded
-                .allowAllAccess(true)
-                .allowExperimentalOptions(true)
-                .build()) {
 
-            // Value bindings = ctx.getBindings("java");
-            // Value harness = bindings.getMember("code.Harness");
-            // harness.invokeMember("main", (Object) new Object[]{"badBubbles", "1000", "1"});       
-
-
+        new EspressoLauncher().LaunchFileWithTool(args[0],args[1], args[2], args[3]);
            
-        }
+        
     }
 
     public void LaunchFileWithTool(String javaProgram, String slowdownAmount, String slowdownMethod) {
@@ -36,7 +22,6 @@ public class EspressoLauncher {
             .option("Virtually-Speeding-Tool", "true")
             .option("Virtually-Speeding-Tool.Slowdown-amount", slowdownAmount)
             .option("Virtually-Speeding-Tool.speed-up-Method", slowdownMethod)
-            .option("Virtually-Speeding-Tool.Line-Number", "1")
             .option("java.Classpath", new JSONObject(Files.readString(Path.of("env.json"))).getString("Classes-Path"))
             .option("java.MultiThreaded", "true")
             .option("engine.MultiTier", "true")
@@ -58,5 +43,34 @@ public class EspressoLauncher {
 
         
     }
+
+    public void LaunchFileWithTool(String javaProgram, String slowdownAmount, String slowdownMethod, String lineNumber) {
+        try(Context ctx = Context.newBuilder("java", "js")
+        .option("Virtually-Speeding-Tool", "true")
+        .option("Virtually-Speeding-Tool.Slowdown-amount", slowdownAmount)
+        .option("Virtually-Speeding-Tool.speed-up-Method", slowdownMethod)
+        .option("Virtually-Speeding-Tool.Enable-Line-Slowdown", "true")
+        .option("Virtually-Speeding-Tool.Line-Number", lineNumber)
+        .option("java.Classpath", new JSONObject(Files.readString(Path.of("env.json"))).getString("Classes-Path"))
+        .option("java.MultiThreaded", "true")
+        .option("engine.MultiTier", "true")
+        .option("engine.DynamicCompilationThresholds", "false")
+        .option("engine.SingleTierCompilationThreshold", "253")
+        .option("engine.CompilationFailureAction", "ExitVM")
+        .allowAllAccess(true)
+        .allowExperimentalOptions(true)
+        .build()) {
+
+    Value bindings = ctx.getBindings("java");
+    Value harness = bindings.getMember("tool.Harness");
+    harness.invokeMember("main", (Object) new Object[]{javaProgram, "50", "1", slowdownMethod, new JSONObject(Files.readString(Path.of("env.json"))).getString("write-Path")});       
+
+    }catch (Exception e) {
+        e.printStackTrace();
+        // TODO: handle exception
+    }
+
+    
+}
 }
 
